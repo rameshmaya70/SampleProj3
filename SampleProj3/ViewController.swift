df2692e0aeff7ffea0a277a4fce120d7e68eb41e
rameshmaya70 <ramesh.khanna@altimetrik.com>
2016-01-28 14:24:25 +0530

    Initial Commit

---
+++ SampleProj3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleProj3
+//
+//  Created by Ramesh Khanna on 1/28/16.
+//  Copyright © 2016 Ramesh Khanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
